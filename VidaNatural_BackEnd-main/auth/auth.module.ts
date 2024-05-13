90bbc78dd66395b8c2c67b14755ff9c62f4ba26e
Laura <lalipuelo@gmail.com>
2024-05-13 20:30:33 -0300

    guardado

--- VidaNatural_BackEnd-main/auth/auth.module.ts
+++ VidaNatural_BackEnd-main/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
-import { jwtConstants } from './auth/auth/constants';
+import { jwtConstants } from './auth/constants';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
