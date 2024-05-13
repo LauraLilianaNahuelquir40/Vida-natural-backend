ef9b063261a36639a9003987f2f9a3f6a5669795
Laura <lalipuelo@gmail.com>
2024-05-13 19:56:52 -0300

    guardado terminado

--- VidaNatural_BackEnd-main/auth/auth.module.ts
+++ VidaNatural_BackEnd-main/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { jwtConstants } from './auth/auth/constants';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule,
+  JwtModule.register({
+    global:true,
+    secret: jwtConstants.secret,
+    signOptions: {expiresIn:'1h'},
+  })
+  ],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService]
 })
 export class AuthModule {}
--- VidaNatural_BackEnd-main/auth/auth.service.ts
+++ VidaNatural_BackEnd-main/auth/auth.service.ts
@@ -1,15 +1,23 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService
+    ) {}
   async login(email: string, pass: string): Promise<any> {
     const user = await this.userService.findUser(email);
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
-    const { password, ...rest } = user;
-    return rest;
+    const payload = { sub: user.id, name: user.NombreApellido, email: user.Email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
+
+
